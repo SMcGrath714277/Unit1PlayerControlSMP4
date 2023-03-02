b5a71fdd85a4165ecc973a19fd9b934be127b98b
SMcGrath714277 <714277@student.dusd.net>
2023-03-01 20:18:33 -0800

    Update

--- PlayerControlSMP4/Assets/Scripts/FollowPlayer.cs
+++ PlayerControlSMP4/Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 offset = new Vector3(0, 6, -7);
     // Start is called before the first frame update
+
     void Start()
     {
         
